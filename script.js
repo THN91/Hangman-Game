082dd437b7f664b9cd7f230402a3a35d55c96b11
Taras Honcharuk <taras.goncharuk22@gmail.com>
2020-06-09 14:06:55 +0300

    redid

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const wordEl = document.getElementById('word');
-const wrongLetterEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
-const finalMessae = document.getElementById('final-message');
+const finalMessage = document.getElementById('final-message');
 
-const figureParts = document.querySelectorAll('.figure-part');
-
-const words = ['application', 'programming', 'interface', 'wizzard'];
+const words = ['application', 'programming', 'interface', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
@@ -28,15 +26,6 @@ function displayWord() {
         .join('')}
   `;
 
-    const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
-
-    if (innerWord === selectedWord) {
-        finalMessage.innerText = 'Congratulations! You won! 😃';
-        popup.style.display = 'flex';
-
-        playable = false;
-    }
 }
 
-
-displayWord()
+displayWord();
