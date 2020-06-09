756433d8c50b3b91cbb7ef536a0638af73e8eecd
Taras Honcharuk <taras.goncharuk22@gmail.com>
2020-06-09 14:28:33 +0300

    js

--- script.js
+++ script.js
@@ -25,7 +25,12 @@ function displayWord() {
         )
         .join('')}
   `;
+    const innerWord = wordEl.innerText.replace(/\n/g, '');
 
+    if (innerWord === selectedWord) {
+        finalMessage.innerText = 'Congratulations! You won!';
+        popup.style.display = 'flex'
+    }
 }
 
 displayWord();
